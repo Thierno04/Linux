72533457749d2f3ee54ce929ca39ce150f98f38c
Thierno04 <115568547+Thierno04@users.noreply.github.com>
2022-10-11 21:53:36 +0200

    Update 3_vim.js

--- 3_vim.js
+++ 3_vim.js
@@ -1,3 +1,25 @@
 i - insert
 
 O - insert ont next line
+
+a - insert after a space
+
+Esc - Escape out of any mode
+
+:q! - quit without saving
+
+:wq! - quit and save
+
+r - replace
+
+d - delete
+
+u - undo
+
+x - remove one character
+
+backspaace -
+  
+/ - forward search
+
+? - backward search
