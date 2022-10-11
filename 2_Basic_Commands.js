4358f8595793e7f814e782cb4ea92a078898cb71
Thierno04 <115568547+Thierno04@users.noreply.github.com>
2022-10-11 21:14:57 +0200

    Update and rename Basic_Commands.js to 2_Basic_Commands.js

---
+++ 2_Basic_Commands.js
@@ -0,0 +1,50 @@
+pwd: Print Working Directory.
+
+ls: Voir les contenues ou attributs.
+
+ls -l: Affiche les elements(dossiers ou fichiers, date de modification, la tail sur le disque).
+
+cd: Est la commande qui change la direction c'est-a-dire change le repertoire en cours vers la racine.
+
+cd /home: Envoie vers la racine.
+
+man: Permet de consulter manuellement les details des commandes. (Pour chercher le mot on utilise / et n pour le suivant).
+
+ls -lh: Affiche les elements.
+
+ls -la: Affiche tous les fichiers ou dossiers cachés et se caracterise par le point.
+
+. : Fait reference au dossier courant.
+
+.. Permets de faire reference au dossier parent.
+
+ls -a: Affiche tous les dossiers cachés et les rendent visibles.
+
+mv: Change le nom de l'objet qu'ont veut deplacer. (ex: mv file1 file2).
+
+mkdir: Make directories. (ex: mkdir folder).
+
+rmdir: Permet d'effacer un dossier. (ex: rmdir folder or rm -r folder).
+
+rm -r: Permet d'effacer un dossier ou un fichier. (ex: rm -r folder or rm -r file).
+
+cp: Met le dossier qui va faloire fournir une source et une destination. (ex: cp -r source destination).
+
+ls alh > result.txt : Permet d'envoyer les -alh vers un fichier result.txt.
+
+ls -l result.txt: Permet d'afficher la tail du fichier.
+
+less result.txt: Affiche le resultat d'un ficher mais ce ficher ne peut pas etre modifier et ni ecrire pour quitter on tape la touche q.
+
+ls -alh|less: Utilise le resultat d'une commande pour le passer a une autre commande sans besoin de passer a un fichier.
+
+vim or vi: Editeur du text. (voir les outils i=ecrire, :wq or :wq!= enregistrer et quitter, :q or :q!= quitter ou force a quitter).
+
+
+
+
+
+
+
+
+
