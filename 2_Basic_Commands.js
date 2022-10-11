030bffcd7ad5a7e097546bcb46582a997bfdce87
Thierno04 <115568547+Thierno04@users.noreply.github.com>
2022-10-11 21:31:23 +0200

    Commands basic done

--- 2_Basic_Commands.js
+++ 2_Basic_Commands.js
@@ -22,9 +22,9 @@ ls -a: Show all hidden folders and make them visible.
 
 mv: Changes the name of the object we want to move. (ex: mv file1 file2).
 
-mkdir: Make directories. (eg: mkdir folder).
+mkdir: Make directories. (ex: mkdir folder).
 
-rmdir: Delete a folder. (eg: rmdir folder or rm -r folder).
+rmdir: Delete a folder. (ex: rmdir folder or rm -r folder).
 
 rm -r: Delete a folder or file. (ex: rm -r folder or rm -r file).
 
@@ -40,7 +40,7 @@ ls -alh|less: Use the result of one command to pass to another command without t
 
 vim or vi: Text editor. (see the i=write, :wq or :wq!= save and quit or force quit tools, :q or :q!= quit or force quit tools).
 
-cat: Displays what is inside the file. (e.g. cat textfile.txt).
+cat: Displays what is inside the file. (ex: cat textfile.txt).
 
 head: Displays the first sentence of the file. (ex: head -1 textfile.txt).
 
