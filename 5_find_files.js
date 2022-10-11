af56d4f08d9793279ad2697772838c3bcbf92f00
Thierno04 <115568547+Thierno04@users.noreply.github.com>
2022-10-11 22:31:47 +0200

    find & locale command

---
+++ 5_find_files.js
@@ -0,0 +1,23 @@
+Finding files or directories using find and locate command
+==========================================================
+
+Here I can create a folder and three subfolders and a file in the third subfolder to find my file.
+
+ex: 
+===
+  cd myfoder/folder1/folder2/folder3/myfile.txt
+  
+To find the file just type
+===========
+find . -name myfile.txt
+    or
+find . -type f -name myfile.txt
+    or
+find . -type d -name myfile.txt
+
+another method is enough to install mlocate
+=============
+sudo apt-get update -y
+sudo apt-get install -y mlocate
+rpm -qa | grep mlocate
+locate myfile.txt
